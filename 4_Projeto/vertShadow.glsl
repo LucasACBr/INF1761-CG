2c73b590efde89c61664f4ab3da86aa9a4a4aca1
LucasACBr <lucas@projetoacbr.com.br>
2021-12-12 14:54:12 -0300

    Update vertShadow.glsl

--- 4_Projeto/vertShadow.glsl
+++ 4_Projeto/vertShadow.glsl
@@ -2,11 +2,15 @@
 
 layout(location = 0) in vec4 pos;
 
-uniform mat4 mvp;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+//uniform mat4 mvp;
+//uniform mat4 svp;
+
 
 void main(void)
 {
+	mat4 mvp = proj * view * model;
 	gl_Position = mvp*pos;
 }
-
-
