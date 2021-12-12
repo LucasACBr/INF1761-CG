3c5ffffc8a9019de9635d420981afd442cc45819
LucasACBr <lucas@projetoacbr.com.br>
2021-12-12 14:54:01 -0300

    Update frag.glsl

--- 4_Projeto/frag.glsl
+++ 4_Projeto/frag.glsl
@@ -1,6 +1,6 @@
 #version 460 core
 
-const float ka = 0.4f;
+const float ka = 0.2f;
 uniform vec4 ma; //= vec4(0.2f, 0.2f, 0.2f, 1.0f);
 uniform vec4 md; //= vec4(1.0f, 1.0f, 1.0f, 1.0f);
 uniform vec4 ms; //= vec4(0.5f, 0.5f, 0.5f, 1.0f);
@@ -14,7 +14,7 @@ in data {
 	vec4 stexcoord;
 }f;
 
-uniform sampler2D smile;
+uniform sampler2D map;
 
 out vec4 color;
 
@@ -24,8 +24,8 @@ void main (void)
 	vec3 nnorm = normalize(f.normal);
 	vec3 lnorm = normalize(f.light);
 	float ndotl = dot(nnorm,lnorm);
-	float kd = (1-ka)* max(0,dot(nnorm,lnorm));
-	vec4 texel = textureProj(smile,f.stexcoord);
+	float kd = (1-ka)* max(0,ndotl);
+	vec4 texel = textureProj(map,f.stexcoord);
 	vec3 lcolor = ((1 - texel.a) * vec3(1.0,1.0,1.0) + texel.a * vec3(texel));
 	//vec3 lcolor = vec3(texel);
 	color = vec4(ka * vec3(ma) + kd * lcolor * vec3(md),md.a);
@@ -35,3 +35,20 @@ void main (void)
 		color += vec4(lcolor*vec3(ms)*pow(max(0,dot(refl,vnorm)),shi),1.0);
 	}
 }
+
+
+
+/*	float shadow = 0.0f;
+	vec3 lightCoords = f.stexcoord.xyz/f.stexcoord.w;
+	if (lightCoords.z <= 1.0f)
+	{	
+		lightCoords = (lightCoords + 1.0f)/2.0f;
+		float closestDepth=texture(smile,lightCoords.xy).r;
+		float currentDepth = lightCoords.z;
+
+		if (currentDepth > closestDepth)
+		{
+			shadow=1.0f;
+		}
+	}
+*/
